c290145e3e77e1773ed9e2663074b4ea28d7cb9c
busypenguin <112860511+busypenguin@users.noreply.github.com>
2023-11-11 18:54:33 +0300

    Создана новая функция

--- scripts.py
+++ scripts.py
@@ -9,7 +9,7 @@ import random
 COMMENDATIONS = ['Хорошая работа!', 'Хвалю!', 'Молодец!', 'Как всегда прекрасно!', 'Продолжай стараться!', 'Так дежать']
 
 
-def fix_marks(schoolkid_name):
+def find_schoolkid(schoolkid_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
     except Schoolkid.MultipleObjectsReturned:
@@ -18,35 +18,22 @@ def fix_marks(schoolkid_name):
     except Schoolkid.DoesNotExist:
         print('Вы ошиблись')
         return
+
+
+def fix_marks(schoolkid_name):
+    schoolkid = find_schoolkid(schoolkid_name)
     child_bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
     child_bad_marks.update(points=5)
 
 
 def remove_chastisements(schoolkid_name):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    except Schoolkid.MultipleObjectsReturned:
-        print('Не удалось найти ученика')
-        return
-    except Schoolkid.DoesNotExist:
-        print('Вы ошиблись')
-        return
+    schoolkid = find_schoolkid(schoolkid_name)
     schoolkid_chastisement = Chastisement.objects.filter(schoolkid=schoolkid)
     schoolkid_chastisement.delete()
 
 
 def create_commendation(schoolkid_name, subject):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    except Schoolkid.MultipleObjectsReturned:
-        print('Не удалось найти ученика')
-        return
-    except Schoolkid.DoesNotExist:
-        print('Вы ошиблись')
-        return
+    schoolkid = find_schoolkid(schoolkid_name)
     certain_lessons = Lesson.objects.order_by(subject__title=subject, year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter)
     certain_lesson = certain_lessons.first()
     Commendation.objects.create(text=random.choice(COMMENDATIONS), created=certain_lesson.date, schoolkid=schoolkid, subject=certain_lesson.subject, teacher=certain_lesson.teacher)
-
-
-create_commendation('Фролов Иван', 'Музыка')
